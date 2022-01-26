deebbed6fcf769b56473fe2726e4b0baaa0ff0c6
ljutaev <ljutaev@gmail.com>
2022-01-27 00:10:49 +0200

    resize logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -18,7 +18,13 @@ class Dom {
     return this
   }
 
-  on() {}
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
+  }
 
   append(node) {
     if (node instanceof Dom) {
@@ -32,6 +38,28 @@ class Dom {
     }
 
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object
+        .keys(styles)
+        .forEach(key => this.$el.style[key] = styles[key])
   }
 }
 
